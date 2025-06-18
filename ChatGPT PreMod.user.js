0aac0ca7625eaab8ec04a0e7e6c6cbc16b8f628a
Raymond Xia <rayzorium@gmail.com>
2025-06-18 11:56:21 -0500

    Update ChatGPT PreMod.user.js

--- ChatGPT PreMod.user.js
+++ ChatGPT PreMod.user.js
@@ -76,8 +76,8 @@
                                         if (!currentMessageId) { // Keep the first we encounter because only one message per request can be unBLOCKED - if user msg is BLOCKED, the response will never show if it's also BLOCKED, so we can safely take the first
                                             currentMessageId = dataObj.message_id;
                                             const requestBody = JSON.parse(args[1].body);
-                                            if (requestBody.message && currentMessageId === requestBody.message.id) {
-                                                accumulatedContent = requestMessage.content.parts[0];
+                                            if (requestBody.messages && currentMessageId === requestBody.messages[0].id) {
+                                                accumulatedContent = requestBody.messages[0].content.parts[0];
                                                 window.alert("Your request was BLOCKED (was still sent, just would be hidden from you if not for this script). It can lead to warning emails and bans, so careful. See README.md for details. Response will not be streamed, and if it also triggers BLOCKED, this script can't save it - you can ask ChatGPT to repeat its response though.");
                                             } else {
                                                 window.alert("The response was BLOCKED, but Premod prevented removal and saved it userscript storage. It will be restored as long as you're on same browser with PreMod enabled. See README.md for details.");
